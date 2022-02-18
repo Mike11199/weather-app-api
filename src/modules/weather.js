ea96bdd2eaa39afff27093fa9a23bd6322ddc58f
Michael <miwanek11199@gmail.com>
2022-02-17 16:53:30 -0800

    switch API to use imperial units not kelvin

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -12,8 +12,8 @@ async function getWeather(locationName) {
     try {
          //don't forget https:// or error.  had to change to https from http due to github pages being https and blocking api request if not also https
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationName + 
-                                          '&APPID=938a422eddf6c0de9dd3c9cc0626c5e4', {mode: 'cors'}) 
-                                          
+                                          '&units=imperial&APPID=938a422eddf6c0de9dd3c9cc0626c5e4', {mode: 'cors'}) 
+
         const weatherData = await response.json();
         console.log(weatherData);
         
