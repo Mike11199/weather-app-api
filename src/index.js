d3ceacc1912f3c134c0e08a02449c39d92a99527
Michael <miwanek11199@gmail.com>
2022-02-18 15:19:41 -0800

    first working version in console

--- src/index.js
+++ src/index.js
@@ -6,23 +6,33 @@ if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
   }
 
-const citySearch = document.getElementById("citySearch");
+
+
 const searchWeather = document.getElementById("searchWeather");
 
-const cityName = citySearch.value;
+
 const cityName2 = "Sacramento";
 
 
+searchWeather.addEventListener('click',async function(e) {
 
-const data = await apiFunctions.getWeather(cityName2);
+ 
+  const data = await apiFunctions.getWeather();
 
-    console.log("The city looked up is " + data.name);
-    console.log("The temperature is " + data.main.temp);
-    console.log("The humidity is " + data.main.humidity);
-    console.log("The actual temperature feels like " + data.main.feels_like);
-    console.log("The visibility in feet is " + data.visibility);
-    console.log("The wind speed is " + data.wind.speed);
-    console.log("The wind heading in degrees is " + data.wind.deg);
+  console.log("The city looked up is " + data.name);
+  console.log("The temperature is " + data.main.temp);
+  console.log("The humidity is " + data.main.humidity);
+  console.log("The actual temperature feels like " + data.main.feels_like);
+  console.log("The visibility in feet is " + data.visibility);
+  console.log("The wind speed is " + data.wind.speed);
+  console.log("The wind heading in degrees is " + data.wind.deg);
+
+});
+
+
+
+
+ 
   
 
 console.log("test3");
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -11,12 +11,17 @@ import regeneratorRuntime from "regenerator-runtime";
 // another function of the object will be to return variables from that data
 
 
+
 //async function here with await keyword instead of regular promise format
-async function getWeather(locationName) {
+async function getWeather() {
     
+
+    const citySearch = document.getElementById("citySearch");
+    const cityName = citySearch.value;
+
     try {
         //don't forget https:// or error.  had to change to https from http due to github pages being https and blocking api request if not also https
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationName + 
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + 
                                         '&units=imperial&APPID=938a422eddf6c0de9dd3c9cc0626c5e4', {mode: 'cors'}) 
 
         const weatherData = await response.json();
