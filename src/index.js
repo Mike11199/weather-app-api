6edc8d29f44264d526b51e0baee99c5bc7b8adff
Michael <miwanek11199@gmail.com>
2022-02-18 14:31:34 -0800

    changed scss

--- src/index.js
+++ src/index.js
@@ -6,18 +6,23 @@ if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
   }
 
+const citySearch = document.getElementById("citySearch");
+const searchWeather = document.getElementById("searchWeather");
 
-const cityName = "Sacramento";
+const cityName = citySearch.value;
+const cityName2 = "Sacramento";
 
-const data = await apiFunctions.getWeather(cityName);
 
-console.log("The city looked up is " + data.name);
-console.log("The temperature is " + data.main.temp);
-console.log("The humidity is " + data.main.humidity);
-console.log("The actual temperature feels like " + data.main.feels_like);
-console.log("The visibility in feet is " + data.visibility);
-console.log("The wind speed is " + data.wind.speed);
-console.log("The wind heading in degrees is " + data.wind.deg);
 
+const data = await apiFunctions.getWeather(cityName2);
+
+    console.log("The city looked up is " + data.name);
+    console.log("The temperature is " + data.main.temp);
+    console.log("The humidity is " + data.main.humidity);
+    console.log("The actual temperature feels like " + data.main.feels_like);
+    console.log("The visibility in feet is " + data.visibility);
+    console.log("The wind speed is " + data.wind.speed);
+    console.log("The wind heading in degrees is " + data.wind.deg);
+  
 
 console.log("test3");
